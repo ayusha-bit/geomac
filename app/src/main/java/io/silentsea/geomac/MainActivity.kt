db2fd5388576cf068bed483f07f1221babee8a90
yurii-shcherbiuk <shcherbiuk.yurii.email@gmail.com>
2025-09-26 11:05:09 +0300

    small fix

--- app/src/main/java/io/silentsea/geomac/MainActivity.kt
+++ app/src/main/java/io/silentsea/geomac/MainActivity.kt
@@ -1,12 +1,11 @@
 package io.silentsea.geomac
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.silentsea.geomac.ui.Geomac
 import io.silentsea.geomac.ui.theme.GeomacTheme
@@ -15,12 +14,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
 
-        Color.Transparent.toArgb().also {
-            enableEdgeToEdge(
-                statusBarStyle = SystemBarStyle.auto(it, it),
-                navigationBarStyle = SystemBarStyle.auto(it, it)
-            )
-        }
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT)
+        )
 
         super.onCreate(savedInstanceState)
 
